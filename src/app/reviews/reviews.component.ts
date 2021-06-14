2e2b45810aa1cc612920e982b05f1e5711f76394
insafml <insaf.mellakh06@gmail.com>
2021-06-14 10:39:42 +0200

    added reviews

--- src/app/reviews/reviews.component.ts
+++ src/app/reviews/reviews.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbRatingConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
-  styleUrls: ['./reviews.component.css']
+  styleUrls: ['./reviews.component.css'],
+  providers:[NgbRatingConfig]
 })
 export class ReviewsComponent implements OnInit {
 
-  constructor() { }
+  constructor(rtconfig: NgbRatingConfig) {
+    rtconfig.max = 5;
+    rtconfig.readonly = true;
+   }
 
   ngOnInit(): void {
   }
