08e3f8bdf8d30705631166e240b00fbc33ab395e
insafml <insaf.mellakh06@gmail.com>
2021-06-16 16:37:04 +0200

    updated hotel details

--- src/app/models/hotel/hotel.ts
+++ src/app/models/hotel/hotel.ts
@@ -9,9 +9,10 @@ export class Hotel {
     src5: string;
     stars: number;
     price: number;
+    description: string;
    
 
-    constructor(id= 0, name= '', location= '', src1= 'src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src2= 'src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src3='src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src4='src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src5='src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', stars= 4, price = 400){
+    constructor(id= 0, name= '', location= '', src1= 'src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src2= 'src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src3='src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src4='src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', src5='src/assets/img/bedrooms/pexels-la-miko-3754595.jpg', stars= 4, price = 400, description=''){
     this.id = id;
     this.name = name;
     this.location = location;
@@ -22,6 +23,7 @@ export class Hotel {
     this.src5 = src5;
     this.stars = stars;
     this.price = price;
+    this.description=description;
    
     }
 }
