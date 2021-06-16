be66fbbd340b440ae3a9f218016b7de11aae9038
insafml <insaf.mellakh06@gmail.com>
2021-06-16 14:37:05 +0200

    reviews update

--- src/app/models/review/review.ts
+++ src/app/models/review/review.ts
@@ -1,9 +1,11 @@
 export class Review {
+    id:number;
     name: string;
     rating: number;
     commentaire: string;
 
-    constructor(name='', rating=0, commentaire=''){
+    constructor(id=0, name='', rating=0, commentaire=''){
+        this.id=id;
 this.name=name;
 this.rating=rating;
 this.commentaire=commentaire;
